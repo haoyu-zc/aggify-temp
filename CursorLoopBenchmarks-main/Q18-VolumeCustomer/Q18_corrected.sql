033c37f58a03d6af46578038fb10442526efae3e
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2023-11-27 21:03:41 -0500

    Create Q18_corrected.sql

---
+++ CursorLoopBenchmarks-main/Q18-VolumeCustomer/Q18_corrected.sql
@@ -0,0 +1,28 @@
+-- Type your code here, or load an example.
+CREATE OR REPLACE FUNCTION VolumeCustomerWithCustomAgg(orderkey bigint)
+    RETURNS int
+    STABLE AS
+$$
+DECLARE
+    ok bigint         := orderkey;
+    i  int            := 0;
+    d  decimal(12, 2) := 0;
+BEGIN
+    if exists (select L_QUANTITY from lineitem 
+                WHERE L_ORDERKEY = ok) then
+        d := (SELECT (L_QUANTITY, d) FROM 
+                (select L_QUANTITY from lineitem 
+                    WHERE L_ORDERKEY = ok) tmp);
+    end if;
+    IF d > 300 THEN
+        i := 1;
+    END IF;
+
+    RETURN i;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Q:
+SELECT O_ORDERKEY, O_TOTALPRICE
+FROM orders
+WHERE VolumeCustomerWithCustomAgg(O_ORDERKEY) = 1;
