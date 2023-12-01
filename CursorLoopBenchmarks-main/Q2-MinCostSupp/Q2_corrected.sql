b149c43ece33ccff4f9997f47c65dd7be5195369
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2023-12-01 00:34:34 -0500

    Update Q2_corrected.sql

--- CursorLoopBenchmarks-main/Q2-MinCostSupp/Q2_corrected.sql
+++ CursorLoopBenchmarks-main/Q2-MinCostSupp/Q2_corrected.sql
@@ -23,6 +23,26 @@ BEGIN
 END;
 $$;
 
+-- Type your code here, or load an example.
+CREATE OR REPLACE FUNCTION MinCostSuppWithCustomAgg(pk BIGINT)
+RETURNS CHAR(25)
+LANGUAGE plpgsql AS $$
+DECLARE
+    key         bigint         := pk;
+    val         CHAR(25);
+    pmin_cost   DECIMAL(25, 2) := 100000;
+BEGIN
+    val := (SELECT case when count(*) > 0 then
+            MinCostSuppAggregate(PS_SUPPLYCOST, S_NAME, pmin_cost) else val end
+            FROM (SELECT PS_SUPPLYCOST, S_NAME
+                                    FROM partsupp,
+                                        supplier
+                                    WHERE PS_PARTKEY = key
+                                    AND PS_SUPPKEY = S_SUPPKEY) tmp);                         
+    RETURN val;
+END;
+$$;
+
 -- Q:
 SELECT P_PARTKEY, MinCostSuppWithCustomAgg(P_PARTKEY)
 FROM part;
