5f74102207c51280515a2c811ac63434929b9947
Yuchen Liu <43634213+hkulyc@users.noreply.github.com>
2023-12-01 00:09:34 -0500

    Update Q14_corrected.sql

--- CursorLoopBenchmarks-main/Q14-PromotionRevenue/Q14_corrected.sql
+++ CursorLoopBenchmarks-main/Q14-PromotionRevenue/Q14_corrected.sql
@@ -17,6 +17,22 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION PromoRevenueWithCustomAgg(partkey INTEGER)
+    RETURNS DECIMAL(25, 5)
+    STABLE AS
+$$
+DECLARE
+    pkey    INTEGER        := partkey;
+    revenue DECIMAL(25, 5) := 0;
+BEGIN
+    revenue := (SELECT case when count(*) > 0 then 
+    promo_revenue_agg(L_EXTENDEDPRICE, L_DISCOUNT, revenue) else revenue end
+    FROM (SELECT L_EXTENDEDPRICE, L_DISCOUNT from lineitem
+    WHERE L_PARTKEY = pkey) tmp);
+    RETURN revenue;
+END;
+$$ LANGUAGE plpgsql;
+
 -- Q:
 SELECT P_PARTKEY, PromoRevenueWithCustomAgg(P_PARTKEY)
 FROM part
